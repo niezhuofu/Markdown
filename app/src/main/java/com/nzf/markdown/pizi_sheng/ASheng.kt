6d0f0d57235601fbfd64738d161aef09468ed510
niezhuofu <ai5680440@qq.com>
2017-10-27 17:19:01 +0800

    clear

--- app/src/main/java/com/nzf/markdown/pizi_sheng/ASheng.kt
+++
@@ -1,9 +0,0 @@
-package com.nzf.markdown.pizi_sheng
-
-import android.content.Context
-import android.view.View
-
-/**
- * Created by joseph on 2017/10/18.
- */
-class ASheng(context: Context?) : View(context) {}
