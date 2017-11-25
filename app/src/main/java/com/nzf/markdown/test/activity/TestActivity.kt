022903a6897f0ea2dd066cbd3ca9fa4700e523b9
niezhuofu <ai5680440@qq.com>
2017-11-25 12:15:45 +0800

    Create Test

---
+++ app/src/main/java/com/nzf/markdown/test/activity/TestActivity.kt
@@ -0,0 +1,9 @@
+package com.nzf.markdown.test.activity
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by niezhuofu on 17-11-25.
+ */
+class TestActivity: AppCompatActivity() {
+}
