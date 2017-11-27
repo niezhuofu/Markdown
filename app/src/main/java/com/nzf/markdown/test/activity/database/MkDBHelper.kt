1a0de4392495a217e84f3d80fed44812cecb1194
josephyuen <615948616@qq.com>
2017-11-27 16:51:07 +0800

    数据库

---
+++ app/src/main/java/com/nzf/markdown/test/activity/database/MkDBHelper.kt
@@ -0,0 +1,41 @@
+package com.nzf.markdown.test.activity.database
+
+import android.annotation.SuppressLint
+import net.sqlcipher.database.SQLiteDatabase
+
+/**
+ * Created by joseph on 2017/11/27.
+ */
+class MkDBHelper(name: String?,version: Int)
+    : BaseDBHelper(name,version) {
+
+    companion object {
+        val DB_VERSION = 1
+
+        val DB_DOWNLOAD = "markdown.db"
+
+        val TABLE_DOWNLOAD_FILE = "file_download"
+
+        val SQL_DOWNLOAD = "create table if not exists ${ TABLE_DOWNLOAD_FILE}(_id integer primary key autoincrement,id integer not null,url text not null,progress text not null,length text not null,_extra1 text default(''),_extra2 text default('')"
+
+        @SuppressLint("StaticFieldLeak")
+        var dbHelper : MkDBHelper? = null
+
+        fun getInstance(){
+            if(dbHelper == null){
+                synchronized(MkDBHelper :: class.java.simpleName){
+                    if(dbHelper == null){
+                        dbHelper = MkDBHelper(DB_DOWNLOAD, DB_VERSION)
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        super.onCreate(db)
+        db!!.execSQL(SQL_DOWNLOAD)
+
+    }
+
+}
