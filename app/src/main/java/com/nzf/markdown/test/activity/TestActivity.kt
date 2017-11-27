d0d3e714c93e1eb51f235d4da87902a9395282a4
josephyuen <615948616@qq.com>
2017-11-27 16:00:26 +0800

    Post请求

--- app/src/main/java/com/nzf/markdown/test/activity/TestActivity.kt
+++ app/src/main/java/com/nzf/markdown/test/activity/TestActivity.kt
@@ -1,9 +1,41 @@
 package com.nzf.markdown.test.activity
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import com.nzf.markdown.R
+import com.nzf.markdown.test.activity.httpconnect.HttpUrlUtils
 
 /**
  * Created by niezhuofu on 17-11-25.
  */
 class TestActivity : AppCompatActivity() {
+     private val TAG = "AppCompatActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_home)
+
+
+
+        Thread(Runnable {
+
+          HttpUrlUtils.Companion.HttpBuilder.builder()
+
+//            run {
+//                while(true){
+//                    Log.i(TAG,"js")
+//                }
+//            }
+
+//            kotlin.run {
+//                while(true){
+//                    Log.i(TAG,"js")
+//                }
+//            }
+        }).start()
+
+
+
+    }
 }
