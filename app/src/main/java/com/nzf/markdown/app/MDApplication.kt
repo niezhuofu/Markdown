c57aef0058440ad354c92768220f82845e7cd88f
josephyuen <615948616@qq.com>
2017-11-27 17:57:14 +0800

    from the beginning

--- app/src/main/java/com/nzf/markdown/app/MDApplication.kt
+++ app/src/main/java/com/nzf/markdown/app/MDApplication.kt
@@ -4,8 +4,6 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
 import android.content.res.Resources
-import com.example.baselib.database.BaseDBHelper
-import com.example.baselib.database.MkDBHelper
 
 /**
  * Created by niezhuofu on 17-11-8.
@@ -23,9 +21,6 @@ class MDApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         mContext = this
-        BaseDBHelper.initDataBase(mContext)
-
-        MkDBHelper.getInstance().getWritableDatabase(BaseDBHelper.SECRET_KEY)
     }
 
 }
--- baselib/src/main/java/com/example/baselib/AC.java
+++
@@ -1,8 +0,0 @@
-package com.example.baselib;
-
-/**
- * Created by joseph on 2017/11/27.
- */
-
-public class AC {
-}
