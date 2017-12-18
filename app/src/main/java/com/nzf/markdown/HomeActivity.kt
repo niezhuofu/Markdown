05467131532b55cefcd74f5d3cb0ea9403f8ed09
niezhuofu <ai5680440@qq.com>
2017-12-18 09:12:30 +0800

    v1.1

--- app/src/main/java/com/nzf/markdown/HomeActivity.kt
+++ app/src/main/java/com/nzf/markdown/HomeActivity.kt
@@ -18,21 +18,6 @@ class HomeActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        initView()
     }
 
-    fun initView(){
-        var llm: LinearLayoutManager = LinearLayoutManager(this)
-        llm.orientation = LinearLayout.VERTICAL
-        rv_home_list.layoutManager = llm
-
-        initAdapter()
-        }
-
-    fun initAdapter(){
-        var fileUtils = FilesUtils.instance
-        var path = fileUtils.getFileDirectory(fileUtils.FILEDIR_EXTERNAL,null)
-        var list : List<MDFileBean>? = fileUtils.showAllMDDir(path!!.path)!!
-
-    }
 }
--- app/src/main/java/com/nzf/markdown/test/activity/TestActivity.kt
+++ app/src/main/java/com/nzf/markdown/test/activity/TestActivity.kt
@@ -1,9 +1,15 @@
 package com.nzf.markdown.test.activity
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 /**
  * Created by niezhuofu on 17-11-25.
  */
 class TestActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 }
